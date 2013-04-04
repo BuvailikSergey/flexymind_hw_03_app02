a823d42ea8f0a48ee3833d13372da1384817ef02
Buvailik Sergey <origamikai@gmail.com>
2013-04-04 21:06:05 +0400

    Made a random location when you press button

--- ButtonRun/src/com/flexymind/buttonrun/MainActivity.java
+++ ButtonRun/src/com/flexymind/buttonrun/MainActivity.java
@@ -1,10 +1,14 @@
 package com.flexymind.buttonrun;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,25 @@ public class MainActivity extends Activity {
 		
 		final RelativeLayout runLayout = (RelativeLayout)findViewById(R.id.runLayout);
 		runLayout.addView(runButton);
+		
+
+		final RelativeLayout.LayoutParams buttonParams = 
+				new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+		
+		runButton.setOnClickListener(new Button.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Random rand = new Random();
+				buttonParams.leftMargin = rand.nextInt(runLayout.getWidth() - 100);
+				buttonParams.topMargin = rand.nextInt(runLayout.getHeight() - 100);
+				
+				runButton.setLayoutParams(buttonParams);
+			}
+			
+		});
 		
 	}
 
